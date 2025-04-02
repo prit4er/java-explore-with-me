3ff831db7eadd62b9c7da5bb763826010a9b8f1f
Aleksey Kolobov <www.kolabs@gmail.com>
2025-04-02 20:16:10 +0300

    Этап 1. Сервис статистики

--- stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
@@ -3,12 +3,7 @@ package ru.practicum.stats;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.HitRequest;
 import ru.practicum.ViewStats;
 
--- stats/stats-server/src/main/java/ru/practicum/stats/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/StatsService.java
@@ -1,11 +1,13 @@
 package ru.practicum.stats;
 
+import org.springframework.stereotype.Service;
 import ru.practicum.HitRequest;
 import ru.practicum.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public interface StatsService {
 
     void hit(HitRequest hitDto);
