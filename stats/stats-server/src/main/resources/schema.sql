646a9220658d9e1114f81d731587acf536fd9638
Aleksey Kolobov <www.kolabs@gmail.com>
2025-04-03 14:30:33 +0300

    fixe

--- stats/stats-server/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE if EXISTS hits CASCADE;
-
-CREATE TABLE IF NOT EXISTS hits(
-    id SERIAL PRIMARY KEY,
-    app varchar(255) NOT NULL,
-    uri varchar(255) NOT NULL,
-    ip varchar(255) NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
-);
